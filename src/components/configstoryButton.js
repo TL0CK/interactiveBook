2f81d44f96e4873b7bf2edc0a033b0dfcd7fabe5
TL0CK <appartement130@gmail.com>
2017-11-22 13:59:52 +0100

    Modification menu

--- src/components/configstoryButton.js
+++ src/components/configstoryButton.js
@@ -4,15 +4,13 @@ class Button extends Component {
 
   render() {
     return (
-      <div>
-        <ul className="menu">
-          <li><a id="btn" class="waves-effect waves-light btn">Epouvante</a></li>
-          <li><a id="btn" class="waves-effect waves-light btn">Fantastique</a></li>
-          <li><a id="btn" class="waves-effect waves-light btn">Jeunesse</a></li>
-          <li><a id="btn" class="waves-effect waves-light btn">Aventure</a></li>
-          <li><a id="btn" class="waves-effect waves-light btn">Erotique</a></li>
-          <li class="bg"></li>
-        </ul>
+      <div className="menu">
+        <a class="waves-effect waves-light btn">Epouvante</a>
+        <a class="waves-effect waves-light btn">Fantastique</a>
+        <a class="waves-effect waves-light btn">Jeunesse</a>
+        <a class="waves-effect waves-light btn">Aventure</a>
+        <a class="waves-effect waves-light btn">Erotique</a>
+        <div class="bg"></div>
       </div>
     );
   }
